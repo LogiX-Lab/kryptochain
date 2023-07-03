5bda03f0992475aa475397170ec619b081d047dd
Ju He <hejunion@gmail.com>
2023-07-04 04:42:19 +0800

    Peer-to-Peer

--- app/index.js
+++ app/index.js
@@ -3,11 +3,14 @@ const BlockChain = require('../blockchain');
 
 const bodyParser = require('body-parser');
 
+const P2PServer = require('./p2p-server');
+
 // HTTP_PORT=3001
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
 const bc = new BlockChain();
+const p2pserver = new P2PServer( bc);
 
 app.use( bodyParser.json());
 
@@ -19,11 +22,15 @@ app.post('/mine', (req, res) =>{
     const block = bc.addBlockData( req.body.data );
     console.log( `mined: ${block.toString() } `);
 
+    p2pserver.syncChains();
+    
     res.redirect('/blocks');
 })
 
 app.listen( HTTP_PORT, () => {
     console.log( `Listening on port ${HTTP_PORT}`);
 })
+
+p2pserver.listen();
 
 module.exports = app;
